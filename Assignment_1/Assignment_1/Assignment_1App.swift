4c7cf71e34131b5c30ef95d3c3efae31f8a89f1a
TedroySimmons <tedroy_simmons@hotmail.com>
2025-02-20 19:41:55 -0500

    testing

---
+++ Assignment_1/Assignment_1/Assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_1App.swift
+//  Assignment_1
+//
+//  Created by Tedroy Simmons on 2025-02-20.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
